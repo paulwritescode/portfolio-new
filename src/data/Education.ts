30d63da72a6d520f2abd867c82b1c145079164a2
paul king <paulkingdev7@gmail.com>
2024-09-03 21:27:15 +0300

    fixed
    fixed

--- src/data/Education.ts
+++ src/data/Education.ts
@@ -1,11 +1,11 @@
-const education = [
-  {
-    degree: "B.Sc Computer Science",
-    institution: "Dedan Kimathi University of Technology",
-    details: [
-      "Core areas: Data structures and algorithms, object-oriented programming, and database design.",
-      "Specialized fields: Internet applications development, Android development, system analysis, human-computer interaction, introduction to machine learning and AI.",
-      "Leadership, Teamwork, and Collaboration.",
-    ],
-  },
-];
+// const education = [
+//   {
+//     degree: "B.Sc Computer Science",
+//     institution: "Dedan Kimathi University of Technology",
+//     details: [
+//       "Core areas: Data structures and algorithms, object-oriented programming, and database design.",
+//       "Specialized fields: Internet applications development, Android development, system analysis, human-computer interaction, introduction to machine learning and AI.",
+//       "Leadership, Teamwork, and Collaboration.",
+//     ],
+//   },
+// ];
